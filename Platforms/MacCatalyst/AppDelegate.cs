ffa8c43b6dda717a4f098ada033e49ccd43c4188
Usuario <Usuario@DESKTOP-D4O88GJ>
2024-10-29 20:52:31 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace hfonsecaS5A
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Views/Principal.xaml.cs
@@ -0,0 +1,9 @@
+namespace hfonsecaS5A.Views;
+
+public partial class Principal : ContentPage
+{
+	public Principal()
+	{
+		InitializeComponent();
+	}
+}
